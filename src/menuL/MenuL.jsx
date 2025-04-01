910e905abf23615555371ed2182e49fb72e85fcc
OrichalcosShunnoros <c3146092091@gmail.com>
2025-04-01 01:31:25 -0500

    overflow hidden added | svg's male and female added

--- src/menuL/MenuL.jsx
+++ src/menuL/MenuL.jsx
@@ -117,8 +117,44 @@ export const MenuL = () => {
       {modalOpen4 && (
         <div className="modal-overlay4" onClick={() => setModalOpen4(false)}>
           <div className="modal-content4" onClick={(e) => e.stopPropagation()}>
-            <button className="genero">♂️</button>
-            <button className="genero">♀️</button>
+            <button className="genero">
+              <svg  
+                xmlns="http://www.w3.org/2000/svg"  
+                width="48"  
+                height="48"  
+                viewBox="0 0 24 24"  
+                fill="none"  
+                stroke="white"  
+                stroke-width="2"  
+                stroke-linecap="round"  
+                stroke-linejoin="round"  
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M10 14m-5 0a5 5 0 1 0 10 0a5 5 0 1 0 -10 0" />
+                <path d="M19 5l-5.4 5.4" />
+                <path d="M19 5h-5" />
+                <path d="M19 5v5" />
+              </svg>
+            </button>
+            
+            <button className="genero">
+              <svg  
+                xmlns="http://www.w3.org/2000/svg"  
+                width="48"  
+                height="48"  
+                viewBox="0 0 24 24"  
+                fill="none"  
+                stroke="white"  
+                stroke-width="2"  
+                stroke-linecap="round"  
+                stroke-linejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M12 9m-5 0a5 5 0 1 0 10 0a5 5 0 1 0 -10 0" />
+                <path d="M12 14l0 7" />
+                <path d="M9 18l6 0" />
+              </svg>
+            </button>
 
             <button className="close4" onClick={() => setModalOpen4(false)}>X</button>
           </div>
