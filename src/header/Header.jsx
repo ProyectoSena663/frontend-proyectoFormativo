c6a347586c36835e20b387b02112d1c4091ced8e
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-10 10:50:16 -0500

    Agregada animacion titulo header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import "./Header.css"
-import {Sun } from 'lucide-react'
+import React from "react";
+import "./Header.css";
+import { Sun } from "lucide-react";
 
 export const Header = () => {
   return (
-    <div className='Header'>
-        <div className='cabecera'>
-        <div className='circulo'></div>
-        <h2>Craft your Style</h2>
-        </div>
-        <div className='divv'><Sun /></div>
-    </div>
-  )
-}
+    <div className="Header">
+      <div className="cabecera">
+        <div className="circulo"></div>
+        <div className="loader">
+          <h2 className="word-container group-1">
+            <span className="word">Craft</span>
+            <span className="word">Shape</span>
+            <span className="word">Build</span>
+            <span className="word">Create</span>
+          </h2>
 
+          <h2 className="word-container group-2">
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+          </h2>
+
+          <h2 className="word-container group-3">
+            <span className="word">Style</span>
+            <span className="word">Vibe</span>
+            <span className="word">Essence</span>
+            <span className="word">Image</span>
+          </h2>
+        </div>{" "}
+      </div>
+      <div className="divv">
+        <Sun />
+      </div>
+    </div>
+  );
+};
