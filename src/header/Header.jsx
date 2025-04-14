87015294ab520c76c8b316dc542a8c8ff820ff81
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-13 19:04:59 -0500

    Diego agregó animación en el titulo de la pagina

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import "./Header.css"
-import {Sun } from 'lucide-react'
-import { NavLink}  from 'react-router-dom'
-
+import React from "react";
+import "./Header.css";
+import { Sun } from "lucide-react";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div className='Header'>
-        <div className='cabecera'>
-          <div className='circulo'></div>
-          <h2>Craft your Style</h2>
-        </div>
-        <nav>
-          <ul className='nav'>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/camisa">Camisa</NavLink>
-            <Sun/> 
-          </ul>
-        </nav>
+    <div className="Header">
+      <div className="cabecera">
+        <div className="circulo"></div>
+        <div className="loader">
+          <h2 className="word-container group-1">
+            <span className="word">Craft</span>
+            <span className="word">Shape</span>
+            <span className="word">Build</span>
+            <span className="word">Create</span>
+          </h2>
+
+          <h2 className="word-container group-2">
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+            <span className="word your">Your</span>
+          </h2>
+
+          <h2 className="word-container group-3">
+            <span className="word">Identity</span>
+            <span className="word">Vibe</span>
+            <span className="word">Essence</span>
+            <span className="word">Image</span>
+          </h2>
+        </div>{" "}
+      </div>
+      <nav>
+        <ul className="nav">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/camisa">Camisa</NavLink>
+          <Sun />
+        </ul>
+      </nav>
     </div>
   );
 };
