aa6f9018b370e606f287a2ecd3607a7804a11dbd
Ordogen <oarcilagarcia@gmail.com>
2025-05-17 18:45:38 -0500

    home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,10 +4,10 @@ export const Home = () => {
 
     return(
         <div className="word-carousel">
-            <span className="word">Craft your Style</span>
-            <span class="word">Crafted Identity</span>
-            <span class="word">Signature Vibe</span>
-            <span class="word">Express Your Vibe</span>
+            <span className="wordd">Craft your Style</span>
+            <span class="wordd">Crafted Identity</span>
+            <span class="wordd">Signature Vibe</span>
+            <span class="wordd">Express Your Vibe</span>
         </div>
     )
 }
