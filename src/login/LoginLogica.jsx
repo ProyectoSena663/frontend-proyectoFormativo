e50fabc5b8ba4b748f79e3cbe3b464021b9b1fdc
DiegoGuayara <diegofguayara9707@gmail.com>
2025-07-17 21:39:52 -0500

    Correccion de logica

--- src/login/LoginLogica.jsx
+++ src/login/LoginLogica.jsx
@@ -1,6 +1,6 @@
 import "./LoginLogica.css";
 import { useState } from "react";
-import {loginUser} from "../api/authApi";
+import { loginUser } from "../api/authApi";
 
 export const LoginLogica = ({ isOpen, onClose }) => {
   const [email, setEmail] = useState("");
@@ -16,24 +16,38 @@ export const LoginLogica = ({ isOpen, onClose }) => {
     }
 
     try {
-      const response = await loginUser.login(email, password);
-      console.log(`bienvenido ${response}`); // Imprimir el mensaje de bienvenida en la consola del navegadorresponse);
+      const response = await loginUser({ email, password });
+      alert(`bienvenido ${response}`); // Imprimir el mensaje de bienvenida en la consola del navegadorresponse);
     } catch (error) {
       alert(`${error.message}`);
     }
-  }
+  };
 
   return (
     <div className="contenedor">
       <div className="contenedorlogin">
-        <button className="closeBTN" onClick={onClose}>X</button>
-        <h2 style={{color:"black"}}>Iniciar Sesión</h2>
+        <button className="closeBTN" onClick={onClose}>
+          X
+        </button>
+        <h2 style={{ color: "black" }}>Iniciar Sesión</h2>
         <form className="formularioLogin">
           <label>Email:</label>
-          <input type="email" placeholder="ejemplo@email.com" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <input
+            type="email"
+            placeholder="ejemplo@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Contraseña:</label>
-          <input type="password" placeholder="********" required value={password} onChange={(e) => setPassword(e.target.value)}/>
-          <button type="submit">Entrar</button>
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={HandleSubmit}>
+            Entrar
+          </button>
         </form>
       </div>
     </div>
